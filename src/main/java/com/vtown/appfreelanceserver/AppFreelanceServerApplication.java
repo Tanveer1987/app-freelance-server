8b41797027afdb1f1bac8a5a0892c5c921c9f941
Tanveer Ahmed <tanveerahmedmp@yahoo.com>
2018-10-25 00:03:37 +0530

    Initial Commit

---
+++ src/main/java/com/vtown/appfreelanceserver/AppFreelanceServerApplication.java
@@ -0,0 +1,12 @@
+package com.vtown.appfreelanceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFreelanceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFreelanceServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vtown/appfreelanceserver/AppFreelanceServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vtown.appfreelanceserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppFreelanceServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
